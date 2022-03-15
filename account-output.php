9d4761caa9b33e4a420d353331529c5f712b1367
20181134 <91303066+20181134@users.noreply.github.com>
2022-03-15 22:28:03 +0900

    bug fixes

--- account-output.php
+++ account-output.php
@@ -10,13 +10,12 @@
     <h1>Create a new Account</h1>
   <?php
   session_start();
-  unset $_SESSION['user'];
   // Upload avatar
   if (is_uploaded_file($_FILES['avatar']['tmp_name'])) {
     if (!file_exists('avatar')) {
       mkdir('avatar');
     }
-    $file='user/'.basename($_FILES['avatar']['tmp_name']);
+    $file='avatar/'.basename($_FILES['avatar']['tmp_name']);
     if (move_uploaded_file($_FILES['avatar']['tmp_name'], $file)) {
       echo 'Uploaded your avatar';
       // Connect to SQL database
@@ -26,13 +25,12 @@
       if ($stmt->execute([$_REQUEST['username'], $_REQUEST['password'], $file])) {
         echo 'Created your account successfully.';
         // Set $_SESSION['user']
-        $pdo2 = new PDO('mysql:host=localhost;dbname=video_sharing;charset=utf8', 'admin', 'password');
-        $stmt2 = $pdo2->prepare('SELECT * FROM video_sharing where username=? and password=?');
+        $stmt2 = $pdo->prepare('SELECT * FROM video_sharing where username=? and password=?');
         if ($stmt2->execute([$_REQUEST['username'], $_REQUEST['password']])) {
           foreach ($stmt2 as $row) {
             $_SESSION['user'] = [
               'username'=>$row['username'],
-              'password'=>$row['password']
+              'avatar'=>$row['avatar']
             ];
           }
         }
@@ -43,8 +41,9 @@
     } else {
       echo 'Could not upload your avatar';
     }
+  } else {
+    echo 'Your avatar was not an uploaded file';
   }
-  echo '<br>';
    ?>
  </main>
  </body>
