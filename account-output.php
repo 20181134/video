cac0fce12720510440d9381d7871c00cb35d34ae
20181134 <91303066+20181134@users.noreply.github.com>
2022-03-19 08:59:19 +0900

    fix

--- account-output.php
+++ account-output.php
@@ -17,13 +17,13 @@
     }
     $file='avatar/'.basename($_FILES['avatar']['tmp_name']).'.jpg';
     if (move_uploaded_file($_FILES['avatar']['tmp_name'], $file)) {
-      echo 'Uploaded your avatar';
+      echo 'Uploaded your avatar<br>';
       // Connect to SQL database
       $pdo = new PDO('mysql:host=localhost;dbname=video_sharing;charset=utf8', 'admin', 'password');
       $stmt = $pdo->prepare('INSERT INTO userdata values(?, ?, ?)');
       // Put information to SQL database
       if ($stmt->execute([$_REQUEST['username'], $_REQUEST['password'], $file])) {
-        echo 'Created your account successfully.';
+        echo 'Created your account successfully.<br>';
         // Set $_SESSION['user']
         $stmt2 = $pdo->prepare('SELECT * FROM video_sharing where username=? and password=?');
         if ($stmt2->execute([$_REQUEST['username'], $_REQUEST['password']])) {
@@ -33,8 +33,11 @@
               'avatar'=>$row['avatar']
             ];
           }
+        }
+        if (isset($_SESSION['user'])) {
+          echo 'Logged in successfully.<br>';
         } else {
-          echo 'Could not log in';
+          echo 'Could not log in.<br>';
         }
       } else {
         echo 'Could not create your account.';
