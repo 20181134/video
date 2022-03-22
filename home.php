494763397aefe16a27361a71a5f18db8576d1750
20181134 <91303066+20181134@users.noreply.github.com>
2022-03-22 19:21:38 +0900

    stylesheet fix

--- home.php
+++ home.php
@@ -7,25 +7,25 @@
     <body>
         <header>
             <div class="logo"></div>
-            <div class="searchbar">
+            <div class="head">
               <form action="search-result.php">
                 <input type="text" name="keyword">
                 <input type="submit" value="Search">
               </form>
+              <div class="links">
+                <a href=./upload.php>Upload</a>
+                <?php
+                session_start();
+                if (isset($_SESSION['user'])) {
+                  echo '<a class="link" href="./user/'.$_SESSION['user']['username'].'.php">My Channel</a>';
+                  echo '<a class="link" href="./signout.php">', $_SESSION['user']['username'], '<a>';
+                } else {
+                  echo '<a class="link" href="./signin.php">Log In</a>';
+                  echo '<a class="link" href="./createaccount.php">Create a new account</a>';
+                }
+                ?>
+              <div>
             </div>
-            <div class="links">
-              <a href=./upload.php>Upload</a>
-              <?php
-              session_start();
-              if (isset($_SESSION['user'])) {
-                echo '<a href="./user/'.$_SESSION['user']['username'].'.php">My Channel</a>';
-                echo '<a href="./signout.php">', $_SESSION['user']['username'], '<a>';
-              } else {
-                echo '<a href="./signin.php">Log In</a>';
-                echo '<a href="./createaccount.php">Create a new account</a>';
-              }
-              ?>
-            <div>
         </header>
         <main>
             <div class="timeline">
